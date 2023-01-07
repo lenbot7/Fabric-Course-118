a6eb7b4c64411c57068752afbae3cf5746200b2f
BuildTools <unconfigured@null.spigotmc.org>
2023-01-07 11:56:02 +0000

    Added Orichalcum Ingot and Orichalcum Nugget

--- src/main/java/net/lenny/mccourse/MCCourseMod.java
+++ src/main/java/net/lenny/mccourse/MCCourseMod.java
@@ -1,6 +1,7 @@
 package net.lenny.mccourse;
 
 import net.fabricmc.api.ModInitializer;
+import net.lenny.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,7 +13,6 @@ public class MCCourseMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
