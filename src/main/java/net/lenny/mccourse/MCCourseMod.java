58a747e9bf1c4b1b2c4c3ef30ec3c9978c25c427
BuildTools <unconfigured@null.spigotmc.org>
2023-01-07 11:10:22 +0000

    Added a Comment

--- src/main/java/net/lenny/mccourse/MCCourseMod.java
+++ src/main/java/net/lenny/mccourse/MCCourseMod.java
@@ -8,6 +8,7 @@ public class MCCourseMod implements ModInitializer {
 	public static final String MOD_ID = "mccourse";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	// A Comment
 	@Override
 	public void onInitialize() {
 
